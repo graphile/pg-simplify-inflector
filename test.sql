29bf8b90bcfdc2ea36d4c3afa3effebbb83c70bb
Benjie Gillam <benjie@jemjie.com>
2019-01-01 18:08:08 +0000

    Extend schema

--- test.sql
+++ test.sql
@@ -2,11 +2,18 @@ drop schema if exists app_public cascade;
 create schema app_public;
 set search_path to app_public, public;
 
-create table fish(
+create table ponds(
   id serial primary key,
   name text not null
 );
-insert into fish(name) values ('Blub'), ('Bubble'), ('Guber');
+
+create table fish(
+  id serial primary key,
+  pond_id int not null references ponds,
+  name text not null
+);
+insert into ponds (name) values ('Amy');
+insert into fish (pond_id, name) values (1, 'Blub'), (2, 'Bubble'), (3, 'Guber');
 
 create table companies(
   id serial primary key,
