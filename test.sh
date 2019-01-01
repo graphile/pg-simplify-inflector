8ee619e5bb69a1e2414a8daaceff08a2a8cd3757
Benjie Gillam <benjie@jemjie.com>
2019-01-01 18:17:35 +0000

    More convenient command for testing

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 psql simplify -f test.sql
-./node_modules/.bin/postgraphile -c simplify -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
-./node_modules/.bin/postgraphile -c simplify -s app_public --simple-collections both -X --append-plugins "$(pwd)/index.js" --export-schema-graphql ./schema.simplified.graphql
+yarn postgraphile -c simplify -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
+yarn postgraphile -c simplify -s app_public --simple-collections both -X --export-schema-graphql ./schema.simplified.graphql
