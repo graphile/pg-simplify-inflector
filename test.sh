4580e93dba00e86e7ab314748bb7192bb55bf3e9
Benjie Gillam <benjie@jemjie.com>
2019-01-01 18:09:08 +0000

    Add simple collections

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 psql simplify -f test.sql
-./node_modules/.bin/postgraphile -c simplify -s app_public -X --export-schema-graphql ./schema.graphql
-./node_modules/.bin/postgraphile -c simplify -s app_public -X --append-plugins "$(pwd)/index.js" --export-schema-graphql ./schema.simplified.graphql
+./node_modules/.bin/postgraphile -c simplify -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
+./node_modules/.bin/postgraphile -c simplify -s app_public --simple-collections both -X --append-plugins "$(pwd)/index.js" --export-schema-graphql ./schema.simplified.graphql
