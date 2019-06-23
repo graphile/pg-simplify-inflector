7c76731ff635d1165c0483f3cb677cb85c783997
Benjie Gillam <benjie@jemjie.com>
2019-06-23 18:05:57 +0100

    Fix test script

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+set -e -x
 dropdb pg_simplify_inflectors || true
 createdb pg_simplify_inflectors
 psql pg_simplify_inflectors -f test.sql
-yarn postgraphile -c pg_simplify_inflectors -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
-yarn postgraphile -c pg_simplify_inflectors -s app_public --simple-collections both -X --export-schema-graphql ./schema.simplified.graphql
+npx postgraphile -c pg_simplify_inflectors -s app_public --simple-collections both -X --export-schema-graphql ./schema.graphql
+npx postgraphile -c pg_simplify_inflectors -s app_public --simple-collections both -X --append-plugins "`pwd`/index.js" --export-schema-graphql ./schema.simplified.graphql
+if command -v colordiff; then
+  COLORDIFF="colordiff"
+else
+  COLORDIFF="cat"
+fi;
+diff -u ./schema.graphql ./schema.simplified.graphql | $COLORDIFF
