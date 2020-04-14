4151621e75fdb8bd44ed20853e5930bb788f16a9
Benjie Gillam <benjie@jemjie.com>
2020-04-14 13:21:11 +0100

    Expected changes from smart comment

--- tests/issue_29/schema.sql
+++ tests/issue_29/schema.sql
@@ -7,4 +7,7 @@ create table something_data(
   something_data_id integer not null primary key,
 	something_id integer not null references something on delete cascade,
 	data text
-)
+);
+
+comment on column something.something_id is E'@name some_id';
+
